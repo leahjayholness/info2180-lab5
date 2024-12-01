efede0b5df7bfa67b12a204bd87c9812e9df692b
leahjayholness <6leahjayholness@gmail.com>
2024-12-01 16:49:43 -0500

    edited php

--- world.js
+++ world.js
@@ -24,6 +24,8 @@
 
 $(document).ready(function() {
     var button = $('#lookup');
+    var button2 = $('#lookup_cities');
+    
     button.on('click', function(element){
         element.preventDefault();
         var country = $('#country').val();
@@ -31,7 +33,27 @@ $(document).ready(function() {
         $.ajax('http://localhost:8888/info2180-lab5/world.php', {
             method: 'POST',
             data: {
-            country: country
+            country: country,
+            lookup: ""
+            }
+        }).done(function(response) {
+            var resp = response;
+            console.log(resp);
+            $('#result').html(resp);
+        }).fail(function() {
+            alert('There was a problem with the request.');
+        });
+    });
+
+    button2.on('click', function(element){
+        element.preventDefault();
+        var country = $('#country').val();
+  
+        $.ajax('http://localhost:8888/info2180-lab5/world.php', {
+            method: 'POST',
+            data: {
+            country: country,
+            lookup: "cities"
             }
         }).done(function(response) {
             var resp = response;
